b4b7e61440ce49298ffa822ea25438a1a4d47b8b
Twin Edo <twinedo.dev@gmail.com>
2024-10-27 05:11:34 +0700

    responsive navbar and hero

--- src/components/home/hero/index.tsx
+++ src/components/home/hero/index.tsx
@@ -1,18 +1,22 @@
-import React from 'react'
-import ImgHero from '@/assets/images/bg-hero.png'
-import Image from 'next/image'
+import React from "react";
+import ImgHero from "@/assets/images/bg-hero.png";
+import Image from "next/image";
 import { GoArrowDownLeft } from "react-icons/go";
 
 const HomeHero = () => {
   return (
-    <div className='flex w-full flex-1 relative'>
-        <Image src={ImgHero} width={undefined} height={934} alt='hero' />
-        <div className='w-[125px] h-[125px] flex flex-col justify-center items-center rounded-full bg-gradient-to-br from-[#FF4E83] to-[#FFBB54] px-[27px] py-[24px] absolute top-[210px] right-[41px]'>
-             <div className='text-white text-center font-bold text-[19px] leading-6'>제작<br />문의하기</div>
-             <GoArrowDownLeft color='white' size={20} />
+    <div className="flex w-full flex-1 relative">
+      <Image src={ImgHero} width={undefined} height={934} alt="hero" />
+      <div className="hidden md:flex w-[125px] h-[125px] flex-col justify-center items-center rounded-full bg-gradient-to-br from-[#FF4E83] to-[#FFBB54] px-[27px] py-[24px] absolute top-[210px] right-[41px]">
+        <div className="text-white text-center font-bold text-[19px] leading-6">
+          제작
+          <br />
+          문의하기
         </div>
+        <GoArrowDownLeft color="white" size={20} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeHero
+export default HomeHero;
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -54,7 +54,8 @@ const Navbar = () => {
   return (
     <div className="flex w-full h-[100px] bg-[#000000] md:py-9 justify-center">
       <div className="flex flex-1 flex-row max-w-[1600px] px-5 items-center md:items-start justify-between">
-        <Image src={Logo} width={170} height={28} alt="logo" />
+        <Image src={Logo} width={170} height={28} alt="logo" className="hidden md:flex" />
+        <Image src={Logo} width={121} height={20} alt="logo" className="flex md:hidden" />
         <div className="hidden md:flex flex-row items-center justify-between flex-1 max-w-[872px]">
           {menu.map((item) => (
             <div key={item.id} onClick={() => setSelectedMenu(item.name)}>
