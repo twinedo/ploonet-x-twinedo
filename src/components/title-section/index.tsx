21d3d0d8e059812fc20100f5f115ad9f2609960a
Twin Edo <twinedo.dev@gmail.com>
2024-10-27 16:16:54 +0700

    change size title section dot

--- src/components/title-section/index.tsx
+++ src/components/title-section/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const TitleSection = ({text}: {text: string}) => {
   return (
     <div className="flex flex-row items-start justify-center">
-        <div className="w-4 h-4 bg-gradient-to-r from-[#FF4E83] to-[#FFBB54] rounded-full" />
+        <div className="h-2 w-2 md:w-4 md:h-4 bg-gradient-to-r from-[#FF4E83] to-[#FFBB54] rounded-full" />
         <div className="text-white text-xl md:text-5xl font-bold">{text}</div>
       </div>
   )
